764e7be2c0f5e8bfd64a037f0ac6db24ba8bf771
sansh2356 <sansh2356@gmail.com>
2025-04-21 20:44:51 +0530

    Adding example 1

---
+++ example_client/src/main.rs
@@ -0,0 +1,16 @@
+pub mod client;
+use client::client_side::client_side_fun;
+#[tokio::main]
+
+async fn main() {
+    let result = client_side_fun().await;
+    match result {
+        Ok(_) => {}
+        Err(error) => {
+            println!(
+                "An error occurred while setting up dummy client {:?}",
+                error
+            );
+        }
+    }
+}
