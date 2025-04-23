8d784898734846340c1b1a0dafdd1b539f9568ca
sansh2356 <sansh2356@gmail.com>
2025-04-24 00:59:05 +0530

    Adding state sharing across different processes concurrently

--- example_client/src/main.rs
+++ example_client/src/main.rs
@@ -3,14 +3,16 @@ use client::client_side::client_side_fun;
 #[tokio::main]
 
 async fn main() {
-    let result = client_side_fun().await;
-    match result {
-        Ok(_) => {}
-        Err(error) => {
-            println!(
-                "An error occurred while setting up dummy client {:?}",
-                error
-            );
+    loop {
+        let result = client_side_fun().await;
+        match result {
+            Ok(_) => {}
+            Err(error) => {
+                println!(
+                    "An error occurred while setting up dummy client {:?}",
+                    error
+                );
+            }
         }
     }
 }
--- examples_async/src/main.rs
+++ examples_async/src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc,Mutex};
 use tokio::net::{TcpListener, TcpStream};
 use tracing::{error, info};
 #[tokio::main]
@@ -21,21 +23,32 @@ async fn main() {
         String::from("127.0.0.1:6379")
     );
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let mut dummy_map = HashMap::new();
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         let mut socket_local_address_result = socket.local_addr().unwrap();
         info!(
             "New client has connected at IP - {:?} and PORT - {:?}",
-            socket_local_address_result.ip().to_string(),socket_local_address_result.clone().port()
+            socket_local_address_result.ip().to_string(),
+            socket_local_address_result.clone().port()
         );
-        process(socket).await;
+        let mut db_reference = dummy_map.clone();
+        //Adding spawing for running concurrent processing for each new connection
+        let join_handle = tokio::spawn(async move {
+            process(socket, &mut db_reference).await;
+        });
+        println!("{:?} spawned process id", join_handle.id());
     }
 }
 #[tracing::instrument]
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db_map: &mut HashMap<String, String>) {
     let mut connection = Connection::new(socket);
     if let Some(frame) = connection.read_frame().await.unwrap() {
         info!("GOT FRAME {:?}", frame.to_string());
+        db_map.insert(String::from("frame"), frame.to_string());
+        for i in db_map.iter() {
+            println!("Key is --- {:?} and value is ---- {:?}", i.0, i.1);
+        }
         let response = Frame::Error("unimplemented".to_string());
         connection.write_frame(&response).await.unwrap();
     }
